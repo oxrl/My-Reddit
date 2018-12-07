df1e7f7f28b90eac357e648b375e2cac444ed73d
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-07 02:02:45 -0500

    Se anadio

--- www/js/app.js
+++ www/js/app.js
@@ -2,13 +2,31 @@
 var app = angular.module('starter', ['ionic','angularMoment']);
       app.controller('RedditCtrl',function($scope,$http){
         $scope.posts = [];
-        $http.get('https://www.reddit.com/new/.json')
+        $http.get('https://www.reddit.com/r/gaming/new/.json')
           .success(function(posts){
             angular.forEach(posts.data.children,function(post,key){
               $scope.posts.push(post.data);
               console.log(post);
             });
           });
+        $scope.cargarNuevosPosts = function (){
+          var params2 = {};
+          if($scope.posts.length > 0){
+            params2['after'] = $scope.posts[$scope.posts.length - 1].name;
+
+          }
+          $http.get('https://www.reddit.com/r/gaming/new/.json',{params: params2})
+            .success(function(posts){
+              angular.forEach(posts.data.children,function(post,key){
+                $scope.posts.push(post.data);
+                console.log(post);
+              });
+              $scope.$broadcast('scroll.infiniteScrollComplete');
+            });
+
+        }
+
+
       });
       app.run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
