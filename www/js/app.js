42bf0909b6451969a8f2a577c4d0d4628dee31a8
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-10 21:48:35 -0500

    Se agrego el modulo InAppBrowser y sun funcionalidad para abrir enlaces

--- www/js/app.js
+++ www/js/app.js
@@ -9,7 +9,7 @@ var app = angular.module('starter', ['ionic','angularMoment']);
               console.log(post);
             });
           });
-        $scope.cargarNuevosPosts = function (){
+        $scope.loadOlderPosts = function (){
           var params2 = {};
           if($scope.posts.length > 0){
             params2['after'] = $scope.posts[$scope.posts.length - 1].name;
@@ -25,15 +25,31 @@ var app = angular.module('starter', ['ionic','angularMoment']);
             });
 
         }
+        $scope.loadNewerPosts = function (){
+          var params2 = { 'before' : $scope.posts[0].name};
+          $http.get('https://www.reddit.com/r/gaming/new/.json',{params: params2})
+            .success(function(posts){
+              var newPost = [];
+              angular.forEach(posts.data.children,function(post){
+                newPost.push(post.data);
+              });
+              $scope.posts = newPost.concat($scope.posts);
+              $scope.$broadcast('scroll.refreshComplete');
+            });
 
-
+        }
+        $scope.openLink = function(url) {
+          window.open(url, '_blank');
+        };
       });
       app.run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
           if (window.cordova && window.Keyboard) {
             window.Keyboard.hideKeyboardAccessoryBar(true);
           }
-
+          if (window.cordova && window.cordova.InAppBrowser) {
+            window.open = window.cordova.InAppBrowser.open;
+          }
           if (window.StatusBar) {
             StatusBar.styleDefault();
           }
